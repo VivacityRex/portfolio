3d2db47d5313361c3f80d1594d3cc48290ecb9a5
Vivacity <127121459+VivacityRex@users.noreply.github.com>
2023-06-20 11:25:37 -0400

    updated video heading for responsiveness

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var video = document.getElementById("myVideo");
-
-    video.addEventListener("timeupdate", function() {
-      if (video.currentTime >= 5) {
-        video.pause();
-      }
-    });
+  const video = document.getElementById("myVideo");
+  const title = document.getElementById("floating-title");
+  
+  video.addEventListener("timeupdate", function() {
+    if (video.currentTime >= 4) {
+      video.playbackRate = 0.5;
+      title.style.opacity = 1;
+      title.style.transform = "translateX(0)";
+    }
   });
+  
+  video.addEventListener("timeupdate", function() {
+    if (video.currentTime >= 4) {
+      video.style.opacity = 1 - (video.currentTime - 4) / 2;
+      if (video.style.opacity <= 0) {
+        title.style.transform = "translateX(0)";
+      }
+    }
+  });
+});
