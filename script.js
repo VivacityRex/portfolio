9f5b1ee74e569284c3e2a39c00c8bbfbedc2dc2f
Vivacity <127121459+VivacityRex@users.noreply.github.com>
2023-06-21 12:39:56 -0400

    updated project links in navbar

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
   const video = document.getElementById("myVideo");
   const title = document.getElementById("floating-title");
-  
+  const fadeOutStart = 2;
+  const fadeOutDuration = 1.5;
+
   video.addEventListener("timeupdate", function() {
-    if (video.currentTime >= 4) {
+    if (video.currentTime >= fadeOutStart) {
       video.playbackRate = 0.5;
       title.style.opacity = 1;
       title.style.transform = "translateX(0)";
-    }
-  });
-  
-  video.addEventListener("timeupdate", function() {
-    if (video.currentTime >= 4) {
-      video.style.opacity = 1 - (video.currentTime - 4) / 2;
+      video.style.transition = `opacity ${fadeOutDuration}s`;
+      video.style.opacity = 0;
+      
       if (video.style.opacity <= 0) {
         title.style.transform = "translateX(0)";
       }
